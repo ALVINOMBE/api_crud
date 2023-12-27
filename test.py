4d4286d5127e4813f22249d2e0b9cd4f55665436
ALVINOMBE <2021100222n@psu.palawan.edu.ph>
2023-12-27 13:33:24 +0800

    adding test_adding function and adding test_add function with messing fields

---
+++ test.py
@@ -0,0 +1,46 @@
+import unittest
+import json
+from api import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+
+    def test_add_customer(self):
+        data = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "middle_name": "M",
+            "gender": "Male",
+            "email": "john.doe@example.com",
+            "street_address": "123 Main St",
+            "city": "Cityville",
+            "country": "Countryland"
+        }
+
+        response = self.app.post('/customer', json=data)
+        data = json.loads(response.get_data(as_text=True))
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(data["message"], "customer added successfully")
+
+    def test_add_customer_missing_fields(self):
+        data = {
+            "last_name": "Doe",
+            "gender": "Male",
+            "email": "john.doe@example.com",
+            "street_address": "123 Main St",
+            "city": "Cityville",
+            "country": "Countryland"
+        }
+
+        response = self.app.post('/customer', json=data)
+        data = json.loads(response.get_data(as_text=True))
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("Error", data)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
